c8615116210e7c033bdb9fd2ba58d6a952385bbe
nadhiw <nwoliye@gmail.com>
2021-07-14 23:46:03 -0500

    add comment

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,6 +8,7 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        // get a user by username
         me: async (parent, args, context) => {
             if (context.user) {
                 const userData = await User.findOne({ _id: context.user._id })
